a6e634ce6bd466ad2174976164cee47376cee797
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-02 20:00:36 +0700

    add socket on even

--- Back-end/socket/index.js
+++ Back-end/socket/index.js
@@ -1,14 +1,52 @@
-const socketIo = require('socket.io')
+const socketIo = require("socket.io");
+
+const users = new Map();
 
 const SocketServer = (server) => {
-    const io = socketIo(server)
+  const io = socketIo(server);
 
-    io.on('connection', (socket)=> {
-        socket.on('join', async(user) => {
-            console.log("new user join: ", user.firstName);
-            io.to(socket.id).emit('typing','User typing...')
-        })
-    })
-}
+  io.on("connection", (socket) => {
+    socket.on("join", async (user) => {
+      let sockets = [];
 
-module.exports = SocketServer
+      if (users.has(user.id)) {
+        const existingUser = users.get(user.id);
+        existingUser.sockets = [...existingUser.socket, ...[socket.id]];
+        users.set(user.id, existingUser);
+        sockets = [...existingUser.socket, ...[socket.id]];
+      } else {
+        users.set(user.id, { id: user.id, sockets: [socket.id] });
+        sockets.push(socket.id);
+      }
+
+      const onlineFriends = []; //ids
+
+      const chatters = []; //query
+
+      // notify user online
+
+      for (let i = 0; i < chatters.length; i++) {
+        if (users.has(chatters[i])) {
+          const chatter = user.get(chatters[i]);
+          chatters.sockets.forEach((sockets) => {
+            try {
+              io.to(socket).emit("online", user);
+            } catch (e) {}
+          });
+          onlineFriends.push(chatter.id);
+        }
+      }
+
+      // send to user sockets which of his friends are online
+      sockets.forEach((socket) => {
+        try {
+          io.to(socket).emit("friends", onlineFriends);
+        } catch (e) {}
+      });
+
+      io.to(socket.id).emit("typing", "User typing...");
+    });
+  });
+};
+
+module.exports = SocketServer;
