1fabf9450d5ebd7c539d01d2b094b62647451099
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-02 09:38:17 +0700

    middleware

---
+++ Back-end/controllers/userController.js
@@ -0,0 +1,6 @@
+const User = require("../models").User;
+const sequelize = require("sequelize");
+
+exports.update = async (req, res) => {
+  return res.send("user controller");
+};
--- front-end/src/components/Chat/components/Navbar/Navbar.js
+++ front-end/src/components/Chat/components/Navbar/Navbar.js
@@ -1,22 +1,56 @@
 import "./Navbar.scss";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { RiArrowDownSFill } from "react-icons/ri";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../../../../store/actions/auth";
+import Modal from "../../../Modal/Modal";
 
 const Navbar = () => {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const user = useSelector((state) => state.authReducer.user);
-  const [showToggle,setShowToggle] = useState(false)
+  const [showToggle, setShowToggle] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
 
   const handleClick = () => {
-    setShowToggle(!showToggle)
-  }
+    setShowToggle(!showToggle);
+  };
 
   const handleLogout = () => {
-    dispatch(logout())
-  }
+    dispatch(logout());
+  };
+
+  const [state, setState] = useState({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    gender: user.gender,
+    email: user.email,
+    password: "",
+   
+  });
+
+  const [avatar, setAvatar] = useState('')
+  
+
+  const { firstName, lastName, gender, email, password } = state;
+
+  const handleChange = (name) => (e) => {
+    setState({
+      ...state,
+      [name]: e.target.value,
+    });
+  };
+
+  const clickSubmit = (e) => {
+    e.preventDefault();
+
+    const form = {firstName, lastName, gender, email, password , avatar}
+    const formData = new FormData()
+    for(const key in form) {
+      formData.append(key, form[key])
+    }
+
+    //dispatch action
+  };
 
   return (
     <>
@@ -28,13 +62,91 @@ const Navbar = () => {
             Yo! {user.firstName} <RiArrowDownSFill size={40} />
           </p>
 
-         {
-           showToggle && 
-           <div className="navbar__profile__dropdown">
-           <p className="navbar__profile__dropdown__text">Update profile</p>
-           <p onClick={handleLogout} className="navbar__profile__dropdown__text">Logout</p>
-         </div>
-         }
+          {showToggle && (
+            <div className="navbar__profile__dropdown " onClick={handleClick}>
+              <p
+                onClick={() => setShowProfile(true)}
+                className="navbar__profile__dropdown__text"
+              >
+                Update profile
+              </p>
+              <p
+                onClick={handleLogout}
+                className="navbar__profile__dropdown__text"
+              >
+                Logout
+              </p>
+            </div>
+          )}
+
+          {showProfile && (
+            <Modal click={() => setShowProfile(false)}>
+              <Fragment key="header">
+                <h3 className="m-0">Update Profile</h3>
+              </Fragment>
+
+              <Fragment key="body">
+                <form className="navbar__form">
+                  <div className="navbar__form__input__field mb-1">
+                    <input
+                      type="text"
+                      value={firstName}
+                      onChange={handleChange("firstName")}
+                      required='required'
+                      placeholder="First name"
+                    />
+                  </div>
+                  <div className="navbar__form__input__field mb-1">
+                    <input
+                      type="text"
+                      value={lastName}
+                      onChange={handleChange("lastName")}
+                      required='required'
+                      placeholder="Last name"
+                    />
+                  </div>
+                  <div className="navbar__form__input__field mb-1">
+                    <select value={gender} onChange={handleChange("gender")}>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                    </select>
+                  </div>
+                  <div className="navbar__form__input__field mb-1">
+                    <input
+                      type="email"
+                      value={email}
+                      onChange={handleChange("email")}
+                      required='required'
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="navbar__form__input__field mb-2">
+                    <input
+                      type="password"
+                      value={password}
+                      onChange={handleChange("password")}
+                      required='required'
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="navbar__form__input__field mb-2">
+                    <input
+                      type="file"
+                     
+                      onChange={e => setAvatar(e.target.files[0])}
+                     
+                    />
+                  </div>
+                </form>
+              </Fragment>
+
+              <Fragment key="footer">
+                <button className="navbar__form__input__field__btn" onClick={clickSubmit}>
+                  UPDATE
+                </button>
+              </Fragment>
+            </Modal>
+          )}
         </div>
       </div>
     </>
