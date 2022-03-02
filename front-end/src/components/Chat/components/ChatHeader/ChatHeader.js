31114e975f842c022951e4d2e8d2a957394c8abb
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-02 18:23:46 +0700

    Chat header + display information

--- front-end/src/components/Chat/components/ChatHeader/ChatHeader.js
+++ front-end/src/components/Chat/components/ChatHeader/ChatHeader.js
@@ -1,11 +1,59 @@
-import "./ChatHeader.scss"
+import "./ChatHeader.scss";
+import { userStatus } from "../../../../utils/helpers";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { TiUserDelete } from "react-icons/ti";
+import { AiOutlineUserAdd, AiTwotoneDelete } from "react-icons/ai";
+import { Fragment, useState } from "react";
 
-const ChatHeader = ({chat}) => {
-   return (
-       <>
-       <h1>hts</h1>
-       </>
-   )
-}
- 
+const ChatHeader = ({ chat }) => {
+  const [showChat, setShowChat] = useState(false);
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [showLeave, setShowLeave] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
+
+  const handleClick = () => {
+    setShowChat(!showChat);
+  };
+
+  return (
+    <Fragment>
+      <div id="chatter">
+        {chat.Users.map((user) => {
+          return (
+            <div key={user.id} className="chatter-info">
+              <h3>
+                {user.firstName}
+                {user.lastName}
+              </h3>
+              <div className="chatter-status">
+                <span className={`online-status ${userStatus(user)}`}> </span>
+              </div>
+            </div>
+          );
+        })}
+        <BsThreeDotsVertical size={25} onClick={handleClick} />
+
+        {showChat ? (
+          <div id="settings">
+            <div className="Option">
+              <AiOutlineUserAdd size={20} />
+              <p>Add user to chat</p>
+            </div>
+            {chat.type === "group" ? (
+              <div className="Option">
+                <TiUserDelete size={20} />
+                <p>Leave chat</p>
+              </div>
+            ) : null}
+            <div className="Option">
+              <AiTwotoneDelete size={20} />
+              <p>Delete chat</p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </Fragment>
+  );
+};
+
 export default ChatHeader;
