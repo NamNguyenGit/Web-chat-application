f408dab025c94044bf56005159e755b59b4eaf42
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-04 09:43:37 +0700

    Create ADd friends modal

--- front-end/src/components/Chat/components/FriendList/FriendList.js
+++ front-end/src/components/Chat/components/FriendList/FriendList.js
@@ -1,6 +1,8 @@
 import { useSelector, useDispatch } from "react-redux";
 import Friends from "../Friends/Friends";
 import { setCurrentChat } from "../../../../store/actions/chat";
+import { useState,Fragment } from "react";
+import Modal from "../../../Modal/Modal+"
 import "./FriendList.scss";
 
 const FriendList = () => {
@@ -8,10 +10,21 @@ const FriendList = () => {
   const dispatch = useDispatch();
 
   const chats = useSelector((state) => state.chatReducer.chats);
+
+  const [showFriendsModal,setShowFriendsModal] = useState(false)
+  const [suggestion,setSuggestion] = useState([])
   
   const openChat = (chat) => {
     dispatch(setCurrentChat(chat));
   };
+
+  const searchFriends = (e) => {
+    // chat service
+  }
+
+  const addNewFriends = (id) => {
+    //dispatch
+  }
 
   return (
     <>
@@ -36,6 +49,31 @@ const FriendList = () => {
             <p id="no-chat">no friends</p>
           )}
         </div>
+        {
+          showFriendsModal && 
+          <Modal >
+            <Fragment key='header'>
+                <h3 className="m-0">Create new chat</h3>
+            </Fragment>
+            <Fragment key='body'>
+              <p>Find friends by name</p>
+              <input type="text" placeholder="Search..."
+            onInput={(e) => searchFriends(e)} 
+            />
+            <div id="suggestions">
+              {
+                suggestion.map(user => {
+                  return <div className="suggestion">
+                        <p className="m-0">{user.FirstName} {user.LastName}</p>
+                        <button onClick={() => addNewFriends(user.id)}>ADD</button>
+                  </div>
+                })
+              }
+
+            </div>
+            </Fragment>
+          </Modal>
+        }
       </div>
     </>
   );
